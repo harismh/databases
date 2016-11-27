cc73f0a98346aff000f6ae2e29675e04196e71db
Haris Muhammad <harismh@icloud.com>
2016-11-26 19:00:02 -0800

    implemented models functions

--- server/models/index.js
+++ server/models/index.js
@@ -2,14 +2,27 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      var string = 'SELECT messages.id, messages.text, messages.roomname, users.username FROM messages \
+                    INNER JOIN users ON (messages.userid = users.id) \
+                    ORDER BY messages.id DESC';
+      db.query(string, (error, results) => callback(error, results));
+    },
+    post: function (data, callback) {
+      var string = 'INSERT INTO messages(text, userid, roomname) \
+                    VALUE (?, (SELECT id FROM users WHERE username = ? LIMIT 1), ?)';
+      db.query(string, data, (error, results) => callback(error, results));
+    }
   },
 
   users: {
-    // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      var string = 'SELECT * FROM users';
+      db.query(string, (error, results) => callback(error, results));
+    },
+    post: function (data, callback) {
+      var string = 'INSERT INTO users(username) VALUE (?)';
+      db.query(string, data, (error, results) => callback(error, results));
+    }
   }
 };
-
