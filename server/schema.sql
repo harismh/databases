f3f989981e3c738e23bd7b87dbb313c19180c8dc
Haris Muhammad <harismh@icloud.com>
2016-11-26 18:09:27 -0800

    updated schema

--- server/schema.sql
+++ server/schema.sql
@@ -3,39 +3,25 @@ CREATE DATABASE chat;
 
 USE chat;
 
-/* Create other tables and define schemas for them here! */
-DROP TABLE IF EXISTS users;
-CREATE TABLE `users` (
+CREATE TABLE users (
   `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(50) DEFAULT NULL,
+  `username` VARCHAR(50) NOT NULL,
 
   UNIQUE(`username`),
   PRIMARY KEY (`id`)
 );
 
-DROP TABLE IF EXISTS chatroom;
-CREATE TABLE chatroom (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `roomname` VARCHAR(50) DEFAULT NULL,
-
-  UNIQUE(`roomname`),
-  PRIMARY KEY(`id`)
-);
-
-DROP TABLE IF EXISTS messages;
 CREATE TABLE messages (
-  /* Describe your table here.*/
   `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `text` VARCHAR(255) DEFAULT NULL,
-  `userid` INTEGER DEFAULT NULL,
-  `roomid` INTEGER DEFAULT NULL,
-  `createdAt` TIMESTAMP,
+  `text` VARCHAR(255) NOT NULL,
+  `userid` INTEGER NOT NULL,
+  `roomname` VARCHAR(50) NOT NULL,  
+  `createdAt` DATETIME DEFAULT CURRENT_TIMESTAMP,
   
   PRIMARY KEY(`id`),
-  FOREIGN KEY(userid) REFERENCES users(`id`),
-  FOREIGN KEY(roomid) REFERENCES chatroom(`id`)
+  FOREIGN KEY(userid) REFERENCES users(`id`)
 );
+
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
-
+ *  to create the database and the tables. */
