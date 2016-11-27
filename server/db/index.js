719468fe761d3fb02a7a9f3dd1fcb72c7b5559ac
Haris Muhammad <harismh@icloud.com>
2016-11-26 19:02:29 -0800

    added database connect function

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,12 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var database = mysql.createConnection({
+  user: 'root',
+  password: '',
+  database: 'chat'
+});
 
+database.connect();
+
+module.exports = database;
